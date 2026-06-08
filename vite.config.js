09706bf1379c0a596cafb0e87c654e37cdf294e0
Itzalem <729185@student.inholland.nl>
2026-06-08 05:10:50 -0400

    aa

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://backend-generation.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
